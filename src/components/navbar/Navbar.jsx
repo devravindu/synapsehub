fad685d70e4c1f9a1a21f05275564dd0c92a09ba
devravindu <hello.devrv@gmail.com>
2024-04-22 23:50:16 +0530

    Update CSS styles and add logo to navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './navbar.css';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.svg'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='synapsehub__navbar'>
+    <div className='synapsehub__navbar-links'>
+      <div className='synapsehub__navbar-links_logo'>
+        <img 
+          src={logo}
+          alt='logo'
+        />
+      </div>
+    </div>
+    </div>
   )
 }
 
