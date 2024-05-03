af5c122183d33bf407d71eb0ae2cbd50034b12e8
devravindu <hello.devrv@gmail.com>
2024-05-04 00:31:55 +0530

    Add new feature to Features component

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -19,6 +19,10 @@ const featuresData = [
     title: "Really boy law county",
     text: "Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush.",
   },
+  {
+    title: "Become the tended active",
+    text: "Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.",
+  },
 ];
 
 const Features = () => {
